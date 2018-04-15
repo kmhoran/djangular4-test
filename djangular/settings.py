0c308ee75055f38261ee2320676f7086a8b850f3
kmhoran <kevin.michael.horan@gmail.com>
2018-04-15 13:13:23 -0700

    pont to new staticfiles folder

--- djangular/settings.py
+++ djangular/settings.py
@@ -133,7 +133,7 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
  
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
  
 # Extra places for collectstatic to find static files.
