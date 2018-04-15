508f4c11952df277e0ba71e14653743465d00890
kmhoran <kevin.michael.horan@gmail.com>
2018-04-15 13:07:21 -0700

    change settings.py

--- djangular/settings.py
+++ djangular/settings.py
@@ -124,12 +124,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
-
-
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
