f01e7115352e7ca3928fdcd4d1ad3fcf8db57b7c
kmhoran <kevin.michael.horan@gmail.com>
2018-04-16 06:42:31 -0700

    add change up static root

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from app import views
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^links/$', views.LinksPageView.as_view()),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
