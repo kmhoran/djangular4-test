e8979623e8efd8435358dd0f6af0f72e2d911164
kmhoran <kevin.michael.horan@gmail.com>
2018-04-16 06:46:38 -0700

    change up url files

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^links/$', views.LinksPageView.as_view()),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
